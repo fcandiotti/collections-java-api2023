4d8ba8ed3019443cb1ca143c4e39fef81c6633cb
FCandiotti <fernandocandiotti@xbrain.com.br>
2023-08-22 15:18:18 -0300

    finaliza desafio conta

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o numero da sua agencia!");
+        int agencia = scanner.nextInt();
+        System.out.println("Digite o numero da sua conta!");
+        String numero = scanner.next();
+        System.out.println("Digite o seu nome");
+        String nomeCliente = scanner.next();
+        System.out.println("Digite seu saldo");
+        BigDecimal saldo = scanner.nextBigDecimal();
+
+        scanner.close();
+
+        System.out.println("Ola " +nomeCliente+ ", obrigado por criar uma conta em nosso banco, sua agencia: " + agencia + 
+        ", conta: " + numero + " e seu saldo R$:" + saldo + " já está disponível para saque!");
+
     }
 }
